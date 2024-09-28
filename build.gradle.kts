f8611237db9c5c178e0fde6ac7f8a54025eb62d0
Elior <heliosaian@gmail.com>
2024-09-28 18:22:12 +0300

    build: Update libraries and plugins versions and configurations, migrate to gradle 2.1 chore: Add .intellijPlatform to .gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,51 +1,152 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import org.jetbrains.changelog.Changelog
+import org.jetbrains.changelog.markdownToHTML
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+fun properties(key: String) = providers.gradleProperty(key).get()
+
+fun fileProperties(key: String) = project.findProperty(key).toString().let { if (it.isNotEmpty()) file(it) else null }
+
+fun environment(key: String) = providers.environmentVariable(key)
+
+
+
 plugins {
   id("java")
-  id("org.jetbrains.kotlin.jvm") version "1.9.24"
-  id("org.jetbrains.intellij") version "2.1.0"
+  alias(libs.plugins.kotlin)
+  alias(libs.plugins.gradleIntelliJPlugin)
+  alias(libs.plugins.changelog)
+  alias(libs.plugins.detekt)
+  alias(libs.plugins.ktlint)
 }
 
-group = "com.mallowigi.permify"
-version = "1.0.2"
+val pluginGroup: String by project
+val pluginName: String by project
+val pluginVersion: String by project
+val pluginSinceBuild: String by project
+val pluginUntilBuild: String by project
+
+val platformVersion: String by project
+
+val javaVersion: String by project
+
+group = pluginGroup
+version = pluginVersion
 
 repositories {
   mavenCentral()
+  mavenLocal()
+  gradlePluginPortal()
+
+  intellijPlatform {
+    defaultRepositories()
+    jetbrainsRuntime()
+  }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-  version.set("LATEST-EAP-SNAPSHOT")
-  type.set("IU") // Target IDE Platform
+dependencies {
+  detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.0")
 
-  plugins.set(
-    listOf(
-      "org.jetbrains.plugins.textmate"
+  intellijPlatform {
+    intellijIdeaUltimate(platformVersion, useInstaller = false)
+    instrumentationTools()
+    pluginVerifier()
+    zipSigner()
+
+    bundledPlugins(
+      "com.intellij.java",
+      "org.jetbrains.plugins.textmate",
     )
-  )
+  }
+}
+
+
+kotlin {
+  jvmToolchain(17)
+}
+
+intellijPlatform {
+  pluginConfiguration {
+    id = pluginGroup
+    name = pluginName
+    version = pluginVersion
+
+    ideaVersion {
+      sinceBuild = pluginSinceBuild
+      untilBuild = pluginUntilBuild
+    }
+
+    changeNotes = provider {
+      with(changelog) {
+        renderItem(
+          (getOrNull(pluginVersion) ?: getUnreleased())
+            .withHeader(false)
+            .withEmptySections(false),
+          Changelog.OutputType.HTML,
+        )
+      }
+    }
+  }
+
+  publishing {
+    token = environment("PUBLISH_TOKEN")
+    channels = listOf(pluginVersion.split('-').getOrElse(1) { "default" }.split('.').first())
+  }
+
+  signing {
+    certificateChain = environment("CERTIFICATE_CHAIN")
+    privateKey = environment("PRIVATE_KEY")
+    password = environment("PRIVATE_KEY_PASSWORD")
+  }
+}
+
+
+changelog {
+  path.set("${project.projectDir}/docs/CHANGELOG.md")
+  version.set(pluginVersion)
+  itemPrefix.set("-")
+  keepUnreleasedSection.set(true)
+  unreleasedTerm.set("Changelog")
+  groups.set(listOf("Features", "Fixes", "Other", "Bump"))
+}
+
+detekt {
+  config.setFrom("./detekt-config.yml")
+  buildUponDefaultConfig = true
+  autoCorrect = true
 }
 
 tasks {
-  // Set the JVM compatibility versions
-  withType<JavaCompile> {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
-  }
-  withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+  javaVersion.let {
+    // Set the compatibility versions to 1.8
+    withType<JavaCompile> {
+      sourceCompatibility = it
+      targetCompatibility = it
+    }
+
+    withType<KotlinCompile> {
+      kotlinOptions.jvmTarget = it
+      kotlinOptions.freeCompilerArgs += listOf("-Xskip-prerelease-check")
+    }
+
+    withType<Detekt> {
+      jvmTarget = it
+      reports.xml.required.set(true)
+    }
   }
 
-  patchPluginXml {
-    sinceBuild.set("241")
-    untilBuild.set("242.*")
+  wrapper {
+    gradleVersion = properties("gradleVersion")
   }
 
-  signPlugin {
-    certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-    privateKey.set(System.getenv("PRIVATE_KEY"))
-    password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+  buildSearchableOptions {
+    enabled = false
   }
 
-  publishPlugin {
-    token.set(System.getenv("PUBLISH_TOKEN"))
+  register("markdownToHtml") {
+    val input = File("./docs/CHANGELOG.md")
+    File("./docs/CHANGELOG.html").run {
+      writeText(markdownToHTML(input.readText()))
+    }
   }
 }
